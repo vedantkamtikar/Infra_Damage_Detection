2e2d7f8e7f6caf015afe194a1b7716ed9cebd24d
vedantkamtikar <vedantkamtikar@gmail.com>
2026-03-30 14:44:53 +0530

    Delete download_roboflow_dataset.py

--- scripts/download_roboflow_dataset.py
+++
@@ -1,9 +0,0 @@
-from roboflow import Roboflow
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent.parent
-
-rf = Roboflow(api_key="Lkl8pHiBySPUXrW5xGli")
-project = rf.workspace("roberta-ruggiero-8apkt").project("cracks-wnd2x")
-version = project.version(2)
-dataset = version.download("yolov8", location=str(ROOT_DIR / "datasets" / "CRACKS"))
